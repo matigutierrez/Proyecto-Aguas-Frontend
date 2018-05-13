33f2b0ef5e2ca8847a60e6606d5498ed3b122c4d
DanielCoronado <d.coronado01@ufromail.cl>
2018-05-13 19:11:39 -0400

    cambio en la ruta a la api

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 angular
   .module('app', ['ngMaterial', 'ngAnimate', 'ngMessages', 'ngAria', 'ui.router', 'ngResource', 'md.data.table', 'pdfjsViewer', 'ngDisableScroll', 'ngStorage'])
-  .constant('API', 'http://localhost:8000/api/');
-  //.constant('API', 'http://50.116.26.42:8000/api/');
+  //.constant('API', 'http://localhost:8000/api/');
+  .constant('API', 'http://50.116.26.42:8000/api/');
